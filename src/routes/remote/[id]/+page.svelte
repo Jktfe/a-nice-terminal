cd90694f8139d04ae05cce609f7358f2d16b8b67
Jktfe <j.w.p.king@gmail.com>
2026-05-01 15:02:48 +0100

    fix(remote): remove stale data warning

--- src/routes/remote/[id]/+page.svelte
+++ src/routes/remote/[id]/+page.svelte
@@ -3,7 +3,7 @@
   import type { PageData } from './$types';
 
   let { data }: { data: PageData } = $props();
-  const room = data.room;
+  const room = $derived(data.room);
 
   type Message = {
     id: string;
