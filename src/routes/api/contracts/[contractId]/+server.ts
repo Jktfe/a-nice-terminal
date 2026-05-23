d45fdbc219c274c2d9c732621c09bf2921d16cd5
Jktfe <j.w.p.king@gmail.com>
2026-05-23 08:48:07 +0100

    Revert "feat(contracts): add public contract pack registry"
    This reverts commit 2c4cf6fafe07955aaacbeebf2eacbacb6c3de3b7.

--- src/routes/api/contracts/[contractId]/+server.ts
+++
@@ -1,9 +0,0 @@
-import { error, json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import { getContractDetail } from '$lib/server/contractPackStore';
-
-export const GET: RequestHandler = async ({ params }) => {
-  const detail = getContractDetail(params.contractId);
-  if (!detail) throw error(404, 'Contract not found.');
-  return json(detail);
-};
